34814bdac8e422c4062d88fb62b772cc5ac26a20
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2023-02-09 16:14:30 -0600

    good % completed

---
+++ src/main/java/com/mohammadrahman/springBootService/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.mohammadrahman.springBootService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    @Autowired
+    Greeting greeting;
+    AtomicLong counter = new AtomicLong();
+    @GetMapping("/greeting")
+    public Greeting  greeting(@RequestParam(value = "name") String name)
+    {
+        greeting.setId(counter.incrementAndGet());
+        greeting.setContent("Hey I am learning Spring Boot from Shetty " + name);
+        return greeting;
+    }
+}
---
+++ src/test/java/com/mohammadrahman/springBootService/SpringBootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammadrahman.springBootService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
