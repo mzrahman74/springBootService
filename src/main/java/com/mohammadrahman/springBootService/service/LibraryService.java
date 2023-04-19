7470dfe8105a986aadcf265b439f4bf9305e14f5
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2023-04-19 17:59:55 -0500

    fix formatting

--- src/main/java/com/mohammadrahman/springBootService/service/LibraryService.java
+++ src/main/java/com/mohammadrahman/springBootService/service/LibraryService.java
@@ -6,26 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-@Service
 
+@Service
 public class LibraryService {
-    @Autowired
-    LibraryRepository repository;
-    public String buildId(String isbn, int aisle) {
-        if(isbn.startsWith("Z")) {
-            return "OLD"+isbn+aisle;
-        }
-        return isbn+aisle;
+  @Autowired LibraryRepository repository;
+
+  public String buildId(String isbn, int aisle) {
+    if (isbn.startsWith("Z")) {
+      return "OLD" + isbn + aisle;
     }
-    public boolean checkBookAlreadyExist(String id) {
-       Optional<Library> lib  =repository.findById(id);
-       if(lib.isPresent()){
-           return true;
-       } else {
-           return false;
-       }
+    return isbn + aisle;
+  }
+
+  public boolean checkBookAlreadyExist(String id) {
+    Optional<Library> lib = repository.findById(id);
+    if (lib.isPresent()) {
+      return true;
+    } else {
+      return false;
     }
-    public Library getBookById(String id) {
-        return repository.findById(id).get();
-    }
+  }
+
+  public Library getBookById(String id) {
+    return repository.findById(id).get();
+  }
 }
--- src/test/java/com/mohammadrahman/springBootService/SpringBootServiceApplicationTests.java
+++ src/test/java/com/mohammadrahman/springBootService/SpringBootServiceApplicationTests.java
@@ -1,4 +1,5 @@
 package com.mohammadrahman.springBootService;
+
 import static org.hamcrest.CoreMatchers.is;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mohammadrahman.springBootService.controller.AddResponse;
@@ -15,11 +16,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import java.util.ArrayList;import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.then;import static org.mockito.Mockito.doNothing;import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -27,77 +32,77 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class SpringBootServiceApplicationTests {
-  @Autowired
-  LibraryController con;
+  @Autowired LibraryController con;
 
-  @MockBean
-  LibraryRepository repository;
-  @MockBean
-  LibraryService libraryService;
-  @Autowired
-  private MockMvc mockMvc;
+  @MockBean LibraryRepository repository;
+  @MockBean LibraryService libraryService;
+  @Autowired private MockMvc mockMvc;
 
   @Test
   void contextLoads() {}
 
-	@Test
-	public void checkBuildIDLogic() {
-		LibraryService lib = new LibraryService();
-		String id = lib.buildId("ZAMAN", 24);
-		assertEquals(id, "OLDZAMAN24");
-		String id1 = lib.buildId("MAN", 24);
-		assertEquals(id1, "MAN24");
-	}
-	@Test
-	public void addBook() {
-	  // mock
-		Library lib = buildLibrary();
-		when(libraryService.buildId(lib.getIsbn(), lib.getAisle())).thenReturn(lib.getId());
-		when(libraryService.checkBookAlreadyExist(lib.getId())).thenReturn(true);
-		when(repository.save(any())).thenReturn(lib);
+  @Test
+  public void checkBuildIDLogic() {
+    LibraryService lib = new LibraryService();
+    String id = lib.buildId("ZAMAN", 24);
+    assertEquals(id, "OLDZAMAN24");
+    String id1 = lib.buildId("MAN", 24);
+    assertEquals(id1, "MAN24");
+  }
 
+  @Test
+  public void addBook() {
+    // mock
+    Library lib = buildLibrary();
+    when(libraryService.buildId(lib.getIsbn(), lib.getAisle())).thenReturn(lib.getId());
+    when(libraryService.checkBookAlreadyExist(lib.getId())).thenReturn(true);
+    when(repository.save(any())).thenReturn(lib);
 
-	 ResponseEntity response = con.addBookImplementation(buildLibrary());
+    ResponseEntity response = con.addBookImplementation(buildLibrary());
     System.out.println(response.getStatusCode());
     assertEquals(response.getStatusCode(), HttpStatus.ACCEPTED);
     AddResponse ad = (AddResponse) response.getBody();
-	ad.getId();
-	assertEquals(lib.getId(), ad.getId());
-	assertEquals("Books already exist", ad.getMsg());
+    ad.getId();
+    assertEquals(lib.getId(), ad.getId());
+    assertEquals("Books already exist", ad.getMsg());
+  }
 
-	}
-
-	@Test
-	public void addBookControllerTest()throws Exception {
-		Library lib = buildLibrary();
-		ObjectMapper map = new ObjectMapper();
-		String jsonString = map.writeValueAsString(lib);
-		when(libraryService.buildId(lib.getIsbn(), lib.getAisle())).thenReturn(lib.getId());
-		when(libraryService.checkBookAlreadyExist(lib.getId())).thenReturn(false);
-		when(repository.save(any())).thenReturn(lib);
+  @Test
+  public void addBookControllerTest() throws Exception {
+    Library lib = buildLibrary();
+    ObjectMapper map = new ObjectMapper();
+    String jsonString = map.writeValueAsString(lib);
+    when(libraryService.buildId(lib.getIsbn(), lib.getAisle())).thenReturn(lib.getId());
+    when(libraryService.checkBookAlreadyExist(lib.getId())).thenReturn(false);
+    when(repository.save(any())).thenReturn(lib);
     this.mockMvc
         .perform(post("/addBook").contentType(MediaType.APPLICATION_JSON).content(jsonString))
-			.andDo(print()).andExpect(status().isCreated())
+        .andDo(print())
+        .andExpect(status().isCreated())
         .andExpect(jsonPath("$.id").value(lib.getId()));
-	}
-	@Test
-	public void getBookByAuthorTest()throws Exception {
-	List<Library> li = new ArrayList<>();
-	li.add(buildLibrary());
-	li.add(buildLibrary());
+  }
 
-	when(repository.findAllByAuthor(any())).thenReturn(li);
-	this.mockMvc.perform(get("/getBooks/author").param("authorname", "Mohammad Rahman"))
-			.andDo(print()).andExpect(status().isOk())
-			.andExpect(jsonPath("$.length()", is(2))).
-					andExpect(jsonPath("$.[0].id").value("fcn322"));
-	}
-	@Test
-	public void updateBookTest()throws Exception {
-	  Library lib = buildLibrary();
-	  ObjectMapper map = new ObjectMapper();
-	  String jsonString = map.writeValueAsString(UpdateLibrary());
-	  when(libraryService.getBookById(any())).thenReturn(buildLibrary());
+  @Test
+  public void getBookByAuthorTest() throws Exception {
+    List<Library> li = new ArrayList<>();
+    li.add(buildLibrary());
+    li.add(buildLibrary());
+
+    when(repository.findAllByAuthor(any())).thenReturn(li);
+    this.mockMvc
+        .perform(get("/getBooks/author").param("authorname", "Mohammad Rahman"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.length()", is(2)))
+        .andExpect(jsonPath("$.[0].id").value("fcn322"));
+  }
+
+  @Test
+  public void updateBookTest() throws Exception {
+    Library lib = buildLibrary();
+    ObjectMapper map = new ObjectMapper();
+    String jsonString = map.writeValueAsString(UpdateLibrary());
+    when(libraryService.getBookById(any())).thenReturn(buildLibrary());
     this.mockMvc
         .perform(
             put("/updateBook/" + lib.getId())
@@ -109,36 +114,39 @@ class SpringBootServiceApplicationTests {
             content()
                 .json(
                     "{\"book_name\":\"Boot\",\"id\":\"fcm321\",\"isbn\":\"fcm\",\"aisle\":321,\"author\":\"Nuhaa Rahman\"}"));
-	}
-	@Test
-	public void deleteBookControllerTest()throws Exception {
-	  when(libraryService.getBookById(any())).thenReturn(buildLibrary());
-	  doNothing().when(repository).delete(buildLibrary());
-    this.mockMvc.perform(
-        delete("/deleteBook")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"id\": \"fcn322\"}")).andDo(print()).andExpect(status().isCreated()).andExpect(content().string("Book is deleted"));
-	}
+  }
 
+  @Test
+  public void deleteBookControllerTest() throws Exception {
+    when(libraryService.getBookById(any())).thenReturn(buildLibrary());
+    doNothing().when(repository).delete(buildLibrary());
+    this.mockMvc
+        .perform(
+            delete("/deleteBook")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\": \"fcn322\"}"))
+        .andDo(print())
+        .andExpect(status().isCreated())
+        .andExpect(content().string("Book is deleted"));
+  }
 
-	public Library buildLibrary() {
-	  Library lib = new Library();
-	  lib.setAisle(322);
-	  lib.setBook_name("Engineering");
-	  lib.setIsbn("fcn");
-	  lib.setAuthor("Yusuf Rahman");
-	  lib.setId("fcn322");
-	  return lib;
+  public Library buildLibrary() {
+    Library lib = new Library();
+    lib.setAisle(322);
+    lib.setBook_name("Engineering");
+    lib.setIsbn("fcn");
+    lib.setAuthor("Yusuf Rahman");
+    lib.setId("fcn322");
+    return lib;
+  }
 
-	}
-	public Library UpdateLibrary() {
-		Library lib = new Library();
-		lib.setAisle(321);
-		lib.setBook_name("Boot");
-		lib.setIsbn("fcm");
-		lib.setAuthor("Nuhaa Rahman");
-		lib.setId("fcm321");
-		return lib;
-
-	}
+  public Library UpdateLibrary() {
+    Library lib = new Library();
+    lib.setAisle(321);
+    lib.setBook_name("Boot");
+    lib.setIsbn("fcm");
+    lib.setAuthor("Nuhaa Rahman");
+    lib.setId("fcm321");
+    return lib;
+  }
 }
